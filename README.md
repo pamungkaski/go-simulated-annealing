b4521b98ca70b2844fde180dc351ee836802e863
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2018-09-23 15:08:29 +0700

    Update readme.md and improve usage on environment variable

--- README.md
+++ README.md
@@ -43,25 +43,49 @@ Please open the detailed file [main.go](https://github.com/pamungkaski/go-simula
 The comment is already tell how the program work.
 
 ## Current Parameter
+
+* Temperature: 100
+
 Temperature is being set become 1000 for more accuration purpose. The changes of temperature will effect the number of iterations
 
-* Temperature: 1000
+* Alpha: 0.8 - 0.95
 
 Alpha is being set become 0.8 to 0.95 to maximize the accuration also. the changes of alpha will effect the number of iterations.
 
-* Alpha: 0.8 - 0.95
+* Minimum Iterations: 1000
 
 Minimum iterations is the number of iterations must be done before doing Temperature cooling.
 
 It being set to be 1000 to increase the accuration of the model
 
-* Minimum Iterations: 1000
+* Goal: -19.208501
+
+The Goal is actually obtained by running The Simulated Annealing with Unlimited Temperatrue.
+The best Solution that I've reached is -19.208501
+
 
 ## Current optimum solution
-* x1:= 8.019960 
-* x2:= 9.668527 
-* E:= -19.196317
-* Accuration:= 99.936577%
+```
+Temperature: 100.00
+Alpha: 0.95
+Minimum Value: -10.00
+Maximum Value: 10.00
+Goal: -19.208501
+Minimum Temperature: 0.0001
+Loop: 10
+fV:= -8.025963 sV:= 9.697703 E:= -19.189334, Accuration:= 99.900214%
+fV:= -7.990458 sV:= 9.825867 E:= -18.902097, Accuration:= 98.404851%
+fV:= -8.010391 sV:= -9.619297 E:= -19.168004, Accuration:= 99.789171%
+fV:= -8.014729 sV:= -9.612083 E:= -19.164561, Accuration:= 99.771247%
+fV:= 8.025812 sV:= 9.530713 E:= -19.022823, Accuration:= 99.033353%
+fV:= -7.889956 sV:= -9.697192 E:= -18.937114, Accuration:= 98.587153%
+fV:= -7.987430 sV:= -9.484198 E:= -18.844195, Accuration:= 98.103413%
+fV:= 8.086753 sV:= 9.655051 E:= -19.197690, Accuration:= 99.943718%
+fV:= -8.119189 sV:= -9.708009 E:= -19.147802, Accuration:= 99.683999%
+fV:= -8.034423 sV:= -9.605722 E:= -19.169468, Accuration:= 99.796792%
+
+```
+> fV = First Value, sV = Second Value, E =  Energy Cost
 
 ## Conclusion
 How the problem(CostFunction) solved by Simmulated Annealing is by picking a random function inside the range.
@@ -72,6 +96,7 @@ every solution will be randomly picked or not picked by a certain probabilty tha
 
 The Data visualization shows that the mainimum value of the problem are located at the very end of every quadrant inside of the cartesian axis.
 It basically tells that the minimum value can be reached on the maximum or minumum value on both x1 and x2.
+
 ## Stack
 * Go <= 1.10.3
 * Already set **$GOPATH** and **$GOROOT**
@@ -87,8 +112,21 @@ It basically tells that the minimum value can be reached on the maximum or minum
  $ cd $GOPATH/src/github.com/pamungkaski/go-simulated-annealing
  $ dep ensure
 ```
+## Environment Variables
+It is located inside .env
+```
+TEMPERATURE: Temperature that used as the params.
+ALPHA: Cooling rate of the Temperature
+MIN_ITERATION: Minimum iteration before start cooling
+MIN_VALUE: Minimum Value of the solution point
+MAX_VALUE: Maximum Value of the solution point
+GOAL: Benchmark of the solution
+MIN_TEMPERATURE: The value that end the algorithm
+LOOP: The number of how many times the Algorithm will run
+```
 
 ## Running the program
+Modify the params by editing the .env file
 ``` 
  $ cd $GOPATH/src/github.com/pamungkaski/go-simulated-annealing
  $ go run app/main.go
--- app/main.go
+++ app/main.go
@@ -6,70 +6,131 @@ import (
 	"math/rand"
 	"time"
 
+	"github.com/joho/godotenv"
+
 	"github.com/pamungkaski/go-simulated-annealing"
+	"os"
+	"strconv"
+	"fmt"
 )
 
 func main() {
-	// Creating Simulated Annealing instance.
-	anne := Simulated.NewAnnealing(20, 0.95, -10, 10, -19.2085, 0.0001)
-	counter := 0
-	solutionCounter := 0
-	minimumIterations := 1000
+	// Input the params from env
+	godotenv.Load()
+	tempString := os.Getenv("TEMPERATURE")
+	alphaString := os.Getenv("ALPHA")
+	minIterationString := os.Getenv("MIN_ITERATION")
+	minValueString := os.Getenv("MIN_VALUE")
+	maxValueString := os.Getenv("MAX_VALUE")
+	goalString := os.Getenv("GOAL")
+	minTempString := os.Getenv("MIN_TEMPERATURE")
+	loopString := os.Getenv("LOOP")
 
-	// Initialization
-	// Seed the random number generator
-	rand.Seed(time.Now().UTC().UnixNano())
-
-	// Intialize the first solution
-	firstValue, secondValue := anne.GenerateNeighbor()
-	currentEnergy := anne.CostFunction(firstValue, secondValue)
-	best := currentEnergy
-
-	// Input the first solution into the struct
-	anne.InputBest(counter)
-	anne.AppendSolution(firstValue, secondValue, currentEnergy)
-
-	// Looping
-	for anne.KeepGoing() {
-		counter++
-		// Generate new solution
-		firstValue, secondValue := anne.GenerateNeighbor()
-		// Generate energy cost
-		energy := anne.CostFunction(firstValue, secondValue)
-		// Calculate delta energy
-		deltaEnergy := energy - currentEnergy
-
-		// If deltaEnergy < 0
-		if deltaEnergy < 0 {
-			// Set the new generated energy as currentEnergy solution
-			currentEnergy = energy
-			solutionCounter++
-			// Save the solution
-			anne.AppendSolution(firstValue, secondValue, energy)
-
-			// If currentEnergy solution better than the best, set the currentEnergy as the best
-			if best > currentEnergy {
-				best = currentEnergy
-				anne.InputBest(solutionCounter)
-			}
-		} else {
-			// Calculate Acceptance Probability
-			probability := anne.AcceptanceProbability(currentEnergy, energy)
-			randomNumber := rand.Float64()
-
-			// If P >= then random number, set the newly generated as curent
-			if probability >= randomNumber {
-				currentEnergy = energy
-			}
-		}
-
-		// Decrease the Temperature if minimumIterations already reached.
-		if counter > minimumIterations {
-			anne.Cooling()
-		}
+	// Parse env into float
+	temp, err := strconv.ParseFloat(tempString, 64)
+	if err != nil {
+		fmt.Errorf("%s", err.Error())
 	}
-	// Print the Best solution
-	anne.BestSolution()
-	// Create Visualization of the model.
-	anne.Visualizing()
+	alpha, err := strconv.ParseFloat(alphaString, 64)
+	if err != nil {
+		fmt.Errorf("%s", err.Error())
+	}
+	minIteration, err := strconv.ParseFloat(minIterationString, 64)
+	if err != nil {
+		fmt.Errorf("%s", err.Error())
+	}
+	minValue, err := strconv.ParseFloat(minValueString, 64)
+	if err != nil {
+		fmt.Errorf("%s", err.Error())
+	}
+	maxValue, err := strconv.ParseFloat(maxValueString, 64)
+	if err != nil {
+		fmt.Errorf("%s", err.Error())
+	}
+	goal, err := strconv.ParseFloat(goalString, 64)
+	if err != nil {
+		fmt.Errorf("%s", err.Error())
+	}
+	minTemp, err := strconv.ParseFloat(minTempString, 64)
+	if err != nil {
+		fmt.Errorf("%s", err.Error())
+	}
+	loop, err := strconv.Atoi(loopString)
+	if err != nil {
+		fmt.Errorf("%s", err.Error())
+	}
+	// Print Params
+	fmt.Printf("Temperature: %.2f\n", temp)
+	fmt.Printf("Alpha: %.2f\n", alpha)
+	fmt.Printf("Minimum Iteration: %.2f\n", minIteration)
+	fmt.Printf("Minimum Value: %.2f\n", minValue)
+	fmt.Printf("Maximum Value: %.2f\n", maxValue)
+	fmt.Printf("Goal: %.6f\n", goal)
+	fmt.Printf("Minimum Temperature: %.4f\n", minTemp)
+	fmt.Printf("Loop: %d\n", loop)
+
+	for i := 0; i < loop; i++{
+		// Creating Simulated Annealing instance.
+		anne := Simulated.NewAnnealing(temp, alpha, minValue, maxValue, goal, minTemp)
+		counter := 0
+		solutionCounter := 0
+		minimumIterations := minIteration
+
+		// Initialization
+		// Seed the random number generator
+		rand.Seed(time.Now().UTC().UnixNano())
+
+		// Intialize the first solution
+		firstValue, secondValue := anne.GenerateNeighbor()
+		currentEnergy := anne.CostFunction(firstValue, secondValue)
+		best := currentEnergy
+
+		// Input the first solution into the struct
+		anne.InputBest(counter)
+		anne.AppendSolution(firstValue, secondValue, currentEnergy)
+
+		// Looping
+		for anne.KeepGoing() {
+			counter++
+			// Generate new solution
+			firstValue, secondValue := anne.GenerateNeighbor()
+			// Generate energy cost
+			energy := anne.CostFunction(firstValue, secondValue)
+			// Calculate delta energy
+			deltaEnergy := energy - currentEnergy
+
+			// If deltaEnergy < 0
+			if deltaEnergy < 0 {
+				// Set the new generated energy as currentEnergy solution
+				currentEnergy = energy
+				solutionCounter++
+				// Save the solution
+				anne.AppendSolution(firstValue, secondValue, energy)
+
+				// If currentEnergy solution better than the best, set the currentEnergy as the best
+				if best > currentEnergy {
+					best = currentEnergy
+					anne.InputBest(solutionCounter)
+				}
+			} else {
+				// Calculate Acceptance Probability
+				probability := anne.AcceptanceProbability(currentEnergy, energy)
+				randomNumber := rand.Float64()
+
+				// If P >= then random number, set the newly generated as curent
+				if probability >= randomNumber {
+					currentEnergy = energy
+				}
+			}
+
+			// Decrease the Temperature if minimumIterations already reached.
+			if counter > minimumIterations {
+				anne.Cooling()
+			}
+		}
+		// Print the Best solution
+		anne.BestSolution()
+		// Create Visualization of the model.
+		anne.Visualizing()
+	}
 }
